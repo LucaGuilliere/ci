2f654a29007cb0169cbabe0e880be33079441692
Luca <luca.guilliere@epitech.eu>
2025-09-29 20:18:53 +0200

    [FIX] jobssss

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.ciapp"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.ciapp"
         minSdk = 30
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
